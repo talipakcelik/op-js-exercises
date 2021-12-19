eef2d2c8fdad729f766f1d8e2c5a3c686d5f53bf
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-20 02:37:40 +0300

    can remove all values kısmında takıldım

--- javascript-exercises/04_removeFromArray/removeFromArray.js
+++ javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr1, num1, num2) {
+  let sum = "";
+  for (i = 0; i < arr1.length; i++) {
+    if (arr1[i] === num1 || (arr1[i] === num2 && typeof num2 !== "Number")) {
+      continue;
+    } else if (arr1[i] !== num1) {
+      sum = sum + arr1[i];
+    }
+  }
+  console.log(typeof sum);
+  return sum.split("").map(Number);
 };
 
 // Do not edit below this line
