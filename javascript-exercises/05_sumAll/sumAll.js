db3eb7ccb6d05a2e554295f5e8ce637c91572052
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-21 17:17:01 +0300

    sumAll tamamlandı

--- javascript-exercises/05_sumAll/sumAll.js
+++ javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let sum = 0;
+  if (
+    typeof num1 !== "number" ||
+    typeof num2 !== "number" ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  } else if (num2 > num1) {
+    for (i = num1; i <= num2; i++) {
+      sum += i;
+    }
+  } else if (num2 < num1) {
+    for (i = num2; i <= num1; i++) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
