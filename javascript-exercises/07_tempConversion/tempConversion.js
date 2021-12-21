2eee20be7a3f5ed48b10018957c2601a76a4e863
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-21 19:53:11 +0300

    tempConversion tamamlandı

--- javascript-exercises/07_tempConversion/tempConversion.js
+++ javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,13 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  return Math.round((temp - 32) * (5 / 9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (temp2) {
+  let sicaklik = temp2 * (9 / 5) + 32;
+  return Math.round(sicaklik * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
