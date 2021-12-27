8d8988fc8b8d87650800ff8d9efc77da06c3e83b
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-28 02:51:05 +0300

    calculator tamamlandı

--- javascript-exercises/08_calculator/calculator.js
+++ javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sumF = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sumF += arr[i];
+  }
+  return sumF;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let multiplyF = 1;
+  for (let i = 0; i < arr.length; i++) {
+    multiplyF *= arr[i];
+  }
+  return multiplyF;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num1) {
+  let result = num1;
+  if (num1 === 0 || num1 === 1) {
+    return 1;
+  } else {
+    while (num1 > 1) {
+      num1--;
+      result *= num1;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +46,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
