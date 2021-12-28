ae773566f78f2681a169c86d65852753eec674ab
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-28 04:28:01 +0300

    Exercise 09 - Palindromes tamamlandı

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const del = /[\W_]/g;
+  const kucukHarfliYap = word.toLowerCase().replace(del, "");
+  const terstenKelime = kucukHarfliYap.split("").reverse().join("");
+  return terstenKelime === kucukHarfliYap;
 };
 
 // Do not edit below this line
