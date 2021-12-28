744446977543c6c8a25a93cdd1ded2c1afc7a7a4
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-28 17:08:41 +0300

    Exercise 12 - Find the Oldest 3 testin hepsi de geçildi

--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,17 +1,27 @@
 const findTheOldest = function (arr) {
+  const currentYear = new Date().getFullYear();
   let theOldest = arr[0];
-  let currentOldest = theOldest.yearOfDeath - theOldest.yearOfBirth;
-  for (let i = 1; i < arr.length; i++) {
-    let incoming = arr[i].yearOfDeath - arr[i].yearOfBirth;
+  let currentOldest = theOldest.hasOwnProperty("yearOfDeath")
+    ? theOldest.yearOfDeath - theOldest.yearOfBirth
+    : currentYear - theOldest.yearOfBirth;
+
+  for (let i = 0; i < arr.length; i++) {
+    let incoming = arr[i].hasOwnProperty("yearOfDeath")
+      ? arr[i].yearOfDeath - arr[i].yearOfBirth
+      : currentYear - arr[i].yearOfBirth;
+
     if (incoming > currentOldest) {
       currentOldest = incoming;
     }
-    console.log(currentOldest);
   }
   const found = arr.find((el) => {
-    return el.yearOfDeath - el.yearOfBirth === currentOldest;
+    const store = el.hasOwnProperty("yearOfDeath")
+      ? el.yearOfDeath - el.yearOfBirth
+      : currentYear - el.yearOfBirth;
+    return store === currentOldest;
   });
   return found;
 };
+
 // Do not edit below this line
 module.exports = findTheOldest;
