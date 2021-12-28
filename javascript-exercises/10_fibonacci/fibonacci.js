7b48fa82de1d6f8956de8434cd99d4a8bcd138e7
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-28 05:17:35 +0300

    Exercise 10 - Fibonacci tamamlandı

--- javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (Number(num) < 0) return "OOPS";
+  else {
+    let fibo = [0, 1];
 
+    for (i = 2; i < Number(num) + 1; i++) {
+      fibo[i] = fibo[i - 2] + fibo[i - 1];
+    }
+    return fibo[Number(num)];
+  }
 };
 
 // Do not edit below this line
