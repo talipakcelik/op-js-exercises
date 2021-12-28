dc211ebf54d6825b58c8153c5018fc8563e49c58
Talip <talipakcelik@hacettepe.edu.tr>
2021-12-28 16:18:45 +0300

    findTheOldest test 1 geçildi

--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  let theOldest = arr[0];
+  let currentOldest = theOldest.yearOfDeath - theOldest.yearOfBirth;
+  for (let i = 1; i < arr.length; i++) {
+    let incoming = arr[i].yearOfDeath - arr[i].yearOfBirth;
+    if (incoming > currentOldest) {
+      currentOldest = incoming;
+    }
+    console.log(currentOldest);
+  }
+  const found = arr.find((el) => {
+    return el.yearOfDeath - el.yearOfBirth === currentOldest;
+  });
+  return found;
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
